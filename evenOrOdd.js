2d65add8636b6a634753848c43b4277c4d901f8b
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-02-16 15:25:03 +0600

    solved problem three: even or odd

---
+++ evenOrOdd.js
@@ -0,0 +1,25 @@
+const n = 0;
+
+// approach 01 
+
+if (n >= 0) {
+//   console.log(n, "is Positive");
+  if (n % 2 === 0) {
+    console.log(n, "is Even");
+  } else {
+    console.log(n, "is Odd");
+  }
+} else {
+  console.log(n, "is Negative");
+}
+
+// console.log(n);
+
+// approach 02
+
+if (n < 0) {
+    console.log("Negative")
+}
+else {
+    console.log()
+}
