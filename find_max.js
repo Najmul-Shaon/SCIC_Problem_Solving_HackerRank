882bc22d8f319b75ef1a66febaab3cf86e6bf3c3
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-02-16 15:00:53 +0600

    solve second problem: find max element

--- find_max.js
+++ find_max.js
@@ -2,4 +2,5 @@ const arr = [12, 7, 2, 6, 0, 8, 99, 1];
 console.log(arr);
 
 const sortedArr = [...arr].sort((a, b) => a - b);
-console.log(sortedArr[sortedArr.length-1]);
+const max = (sortedArr[sortedArr.length-1]);
+ console.log(max)
